c341df389028debd3b00781ff10c3e905ed4f4c5
mynameisi <test@nowhere.com>
2016-02-04 10:18:23 +0800

    1st commit

---
+++ 2016cs36e1e2/2016cs36e1e2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  2016cs36e1e2
+//
+//  Created by wgz on 16/2/4.
+//  Copyright © 2016年 wgz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
